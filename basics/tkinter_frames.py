9d3c734ad1cb37133cb9540e49b50eee767d9269
Alfred Millett-Sikking <amsikking@gmail.com>
2024-10-16 14:59:52 -0700

    Update tkinter_frames.py
    - The keyword "quit" replaced with "close" to avoid confusion since we are using "self.destroy" here and not "self.quit".
    - Added a proper close method so the user can now exit with the built in 'X' button (top right corner), which also executes any chosen functions before destroying the root.

--- basics/tkinter_frames.py
+++ basics/tkinter_frames.py
@@ -13,9 +13,9 @@ class TestFrame(tk.Frame):
             self, text=self.label, command=self.print)
         self.print_button.grid(row=0)
         
-        self.quit_button = tk.Button(
-            self, text="Quit_frame", command=self.destroy)
-        self.quit_button.grid(row=1)
+        self.close_button = tk.Button(
+            self, text="close_frame", command=self.destroy)
+        self.close_button.grid(row=1)
 
     def print(self):
         print(self.label, 'hello')
@@ -30,5 +30,11 @@ frame2 = TestFrame(root, 'Print2', 1)
 quit_root_button = tk.Button(root, text="Quit_root", command=root.quit)
 quit_root_button.grid()
 
+# add close function + any commands for when the user hits the 'X'
+def close():
+    print('Closing')
+    # close root window:
+    root.destroy()
+root.protocol("WM_DELETE_WINDOW", close)
+
 root.mainloop()
-root.destroy()
