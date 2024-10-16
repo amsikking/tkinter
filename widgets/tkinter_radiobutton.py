bc9a34513483ec5ca1e0efba82dd3ea2e512dba2
Alfred Millett-Sikking <amsikking@gmail.com>
2024-10-16 15:09:06 -0700

    Update tkinter_radiobutton.py
    Removed the quit button. The user can now exit with the built in 'X' button (top right corner), which also executes any chosen functions before destroying the root.

--- widgets/tkinter_radiobutton.py
+++ widgets/tkinter_radiobutton.py
@@ -32,8 +32,11 @@ root.title('Radio_button_GUI')
 
 frame = TestFrame(root)
 
-quit_button = tk.Button(root, text="Quit", command=root.quit)
-quit_button.grid(row=3)
+# add close function + any commands for when the user hits the 'X'
+def close():
+    print('Closing')
+    # close root window:
+    root.destroy()
+root.protocol("WM_DELETE_WINDOW", close)
 
 root.mainloop()
-root.destroy()
